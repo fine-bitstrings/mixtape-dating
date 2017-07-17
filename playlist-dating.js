98ce128855263e5e3dbd71793dba9ee1006ea973
Joshua Marshall Moore <thwee.abacadabra.alchemist@gmail.com>
2017-07-17 09:42:26 -0700

    add playlist works/gets a link back. the link does work, but the playlist page doesn't render properly

--- playlist-dating.js
+++ playlist-dating.js
@@ -2,20 +2,33 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
 const app = express();
 
-app.get('/', function(req, res){
-  res.send('Hello, Worlds!');
-});
+app.set('view engine', 'ejs');
+app.set('views', './client');
+
+var playlists = [];
+
 
 app.use('/client', express.static('client'));
 app.use(bodyParser.json());
+app.use(express.static('views'));
 
-app.post('/create-playlist', function(req, res){
-  console.log(req.body.title);
-  console.log(req.body.playlist);
-  console.log(req.body.email);
-  res.send();
+app.get('/', function(req, res){
+  res.render('index.ejs', {playlists: playlists});
+});
+
+app.get('/playlist/:id', function(req, res){
+  console.log('playlist/' + req.params.id, playlists[req.params.id]);
+  res.render('playlist.ejs', {playlist: playlists[req.params.id]});
+});
+
+app.post('/create-playlist/', function(req, res){
+  playlists.push(req.body);
+  var link = (playlists.length-1).toString();
+  playlists[playlists.length-1].url = '/playlist/' + link;
+  res.send(JSON.stringify({url: link}));
 });
 
 
@@ -23,3 +36,12 @@ var PORT = 16000
 app.listen(PORT, function(){
   console.log('App Playlist Dating listening on port ' + PORT);
 });
+
+/* Thanks to:
+https://codeforgeek.com/2014/09/handle-get-post-request-express-4/
+http://regexr.com/
+https://stackoverflow.com/questions/22952044/loop-through-json-in-ejs
+https://www.html5rocks.com/en/tutorials/file/xhr2/
+https://expressjs.com/en/guide/routing.html
+
+*/
